0fa015b333db3eb3fcba326c2612268c7fe4ea34
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-07-16 10:04:03 +0900

    ディレクトリ名の修正

--- backend/app/Http/Controllers/API/TodoController.php
+++ backend/app/Http/Controllers/API/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\API\BaseController;
+use App\Http\Controllers\Api\BaseController;
 use App\Application\Services\TodoService;
 use Illuminate\Http\Request;
 
